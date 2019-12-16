1dcd082c659bc9429070233906d95df0e8d80076
Keyvan Abodollahzadeh <key.one72@gmail.com>
2019-12-16 23:52:11 +0330

    Update LoginModel.cs

--- IrTrainDotNet/Models/LoginModel.cs
+++ IrTrainDotNet/Models/LoginModel.cs
@@ -6,7 +6,7 @@ namespace IRTrainDotNet.Models
 {
   public  class LoginModel
     {
-        public string Username { get; set; };
-        public string Password { get; set; };
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
